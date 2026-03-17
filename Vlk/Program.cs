2f73f38b08b13e490d1a8150d37fe454f09a9f1b
Denis-Rubtsov <dr6750455@gmail.com>
2026-03-17 23:15:11 +0300

    Улучшение UI 2

--- Vlk/Program.cs
+++ Vlk/Program.cs
@@ -140,7 +140,7 @@ class InlineHandler
             cacheTime: 0,
             isPersonal: true,
             null,
-            switchPmText: $"Для получения конкретной цитаты введите целое число от 1 до {_data.Data.quotes.Count}",
+            switchPmText: $"Введите целое число от 1 до {_data.Data.quotes.Count}",
             "start"
         );
     }
