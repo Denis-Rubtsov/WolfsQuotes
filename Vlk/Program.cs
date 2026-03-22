b47ddb958ba4b884dbe0e8773d4816ef223abc57
Denis-Rubtsov <dr6750455@gmail.com>
2026-03-22 23:32:43 +0300

    23

--- Vlk/Program.cs
+++ Vlk/Program.cs
@@ -121,10 +121,10 @@ class InlineHandler
                 index = _quotes.GetRandom();
                 number = index + 1;
                 quote = _data.Data.quotes[index];
-                if (number == 31) number = 0;
             }
 
-            var voice = _voiceUrl + $"{number}.ogg?revision={DateTime.UtcNow.Ticks}";
+            if (number == 31) number = 0;
+            var voice = _voiceUrl + $"{number}.ogg";
             Console.WriteLine(voice);
 
             var results = new InlineQueryResult[]
